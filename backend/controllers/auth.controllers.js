f37eed6b8f04e752dc5caeb271676e4eb960cda3
Efa <efatariku07@gamil.com>
2025-06-20 14:12:13 +0300

    new file:   backend/controllers/auth.controllers.js 	new file:   backend/db/connectToMongoDB.js 	new file:   backend/models/user.model.js 	new file:   backend/routes/auth.routes.js 	new file:   backend/server.js 	new file:   backend/utils/generateToken.js 	new file:   package-lock.json 	new file:   package.json

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,86 @@
+import User from "../models/user.model.js"
+import { v4 as uuid } from 'uuid';
+import bcrypt from "bcrypt"
+import generateToken from "../utils/generateToken.js";
+
+export const signup = async (req,res) => {
+    try{
+        const {fullName,username,password,confirmPassword,gender} = req.body;
+
+        if(password != confirmPassword){
+            return res.status(400).json({error:"password don't match"})
+        }
+
+        const user = await User.findOne({username})
+        if(user){
+            return res.status(400).json({error:`user with username ${username} already exist`})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const avatarSeed = username || uuid()
+        const avatarUrl = `https://api.dicebear.com/7.x/adventurer/svg?seed=${avatarSeed}`
+
+        const newUser = await new User({
+            fullName,
+            username,
+            password:hashedPassword,
+            gender,
+            profilePic:avatarUrl
+        })
+        if(newUser){
+        generateToken(newUser._id,res)
+        await newUser.save()
+        res.status(201).json({
+            _id:newUser._id,
+            fullName:newUser.fullName,
+            username:newUser.username,
+            profilePic:newUser.profilePic
+        })
+    }else{
+        res.status(400).json({
+            error:"Invalid user data"
+        })
+    }
+        
+    }catch(error){
+        res.status(500).json({
+            error:"Internal server error"
+        })
+        console.log(error.message)
+    }
+}
+
+
+export const login = async (req,res) => {
+    try{
+const {username,password} = req.body;
+const user = await User.findOne({username})
+const isPassword = await bcrypt.compare(password,user?.password || "") 
+
+if(!user || !isPassword){
+    res.status(400).json({error:"invalid username or password"})
+}
+generateToken(user._id,res)
+res.status(200).json({
+    _id:user._id,
+    fullName:user.fullName,
+    username:user.username,
+    profilePic:user.profilePic
+})
+    }catch(error){
+console.log(error.message)
+res.status(500).json({error:"internal server error"})
+    }
+}
+
+export const logout = (req,res) => {
+   try{
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({message:"logged out successfully"})
+
+   }catch(error){
+    console.log(error.message)
+res.status(500).json({error:"internal server error"})
+   }
+}
---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        require:true
+    },
+    username:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+        minlength: 6,
+    },
+    gender:{
+        type:String,
+        require:true,
+        enum:["male","female"],
+
+    },
+    profilePic:{
+        type:String,
+        default:"",
+    }
+})
+
+const User = mongoose.model("User",userSchema)
+
+export default User
